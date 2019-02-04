689f8c15e6ef2c14bdcd40461d299550735f171c
Markus Szokoll <markus.szokoll@gmx.at>
2019-02-04 13:31:45 +0100

    grocery hp-files

--- challange/js/index.js
+++ challange/js/index.js
@@ -1,22 +1,12 @@
 $(document).ready(function(){
-$("form").submit(function(e) {
 
-	e.preventDefault();
+
+	var groceryArticles = JSON.parse(grocery_data);
+
 	
-	
+	for(var i= 0; i<groceryArticles.length)
+	document.getElementById("store_articles").innerHTML=groceryArticles;
 
-});
-//$("#myform").remove();
-
-for(var i=1;i<=5;i++){
-	var newItem=$('<div>3</div>');
-	$("article").append(newItem);	
-}
-
-$("#msg").val("Search for...");
-
-$("div").addClass("box");
-
-$("a").attr("href","http://www.codefactory.wien");
+	// document.getElementById("main").addEventListener("click", do_login, false);
 
 });
