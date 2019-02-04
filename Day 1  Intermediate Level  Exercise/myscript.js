b31988f9d0c433c0bb8c5419e6a2ff23dfacd7c7
ahmadtaktak <ahmad.tk129@gmail.com>
2019-02-04 11:21:44 +0100

    intermediate

---
+++ Day 1  Intermediate Level  Exercise/myscript.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+$("form").submit(function(e) {
+
+	e.preventDefault();
+	
+	
+
+});
+//$("#myform").remove();
+var newItem=$('<div>3</div>');
+
+$("article").append(newItem);
+var newItem=$('<div>3</div>');
+$("article").append(newItem);
+var newItem=$('<div>3</div>');
+$("article").append(newItem);
+var newItem=$('<div>3</div>');
+$("article").append(newItem);
+var newItem=$('<div>3</div>');
+$("article").append(newItem);
+
+$("#msg").val("Search for...");
+
+$("div").addClass("box");
+
+$("a").attr("href","http://www.codefactory.wien");
+
+});
